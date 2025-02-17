6513fd797c1ea0e451b2522d6f1460584cf39054
Drenore <benjamin.cristinelli1@gmail.com>
2025-02-17 19:22:49 +0100

    test 4

--- tests/test_backup_script.py
+++ tests/test_backup_script.py
@@ -8,7 +8,6 @@ from backup_script import detect_os, get_home_directory, create_backup_archive
 
 def test_detect_os():
     os_name = detect_os()
-    # On teste que l'OS détecté est parmi ceux attendus.
     assert os_name in ['Linux', 'Windows', 'Darwin']
 
 def test_get_home_directory():
@@ -16,21 +15,21 @@ def test_get_home_directory():
     assert os.path.isdir(home_dir)
 
 def test_create_backup_archive():
-    # Crée un répertoire temporaire avec un fichier de test.
+
     with tempfile.TemporaryDirectory() as temp_dir:
         test_file = os.path.join(temp_dir, 'test.txt')
         with open(test_file, 'w') as f:
             f.write("Hello World")
         
-        # Crée l'archive à partir de ce répertoire temporaire.
+
         archive_base = os.path.join(temp_dir, 'test_backup')
         archive_path = create_backup_archive(temp_dir, archive_base)
         
-        # Vérifie que l'archive a été créée.
+
         assert archive_path is not None
         assert os.path.isfile(archive_path)
         
-        # Vérifie que le fichier test.txt est bien présent dans l'archive.
+
         with zipfile.ZipFile(archive_path, 'r') as zip_ref:
             namelist = zip_ref.namelist()
             # On cherche un chemin se terminant par "test.txt" (il peut être dans un sous-dossier).
